d35704d9ab28ec54837078289bd7664d5a467bb9
Chris Akridas <xrhstosakridas@gmail.com>
2022-07-16 16:27:37 +0300

    i must commit before pull

--- seeds/index.js
+++ seeds/index.js
@@ -13,14 +13,19 @@ mongoose.connect('mongodb://localhost:27017/yelp-camp')
 })
 
 const sample = array => array[Math.floor(Math.random() * array.length)];
-
+const randomPrice = () => {
+  return Math.floor(Math.random() * 20 + 10);
+}
 const seedDB = async () => {
   await Campground.deleteMany({});
   for(let i  = 0; i < 50; i++){
     const random1000 = Math.floor(Math.random() * 1000);
     const camp = new Campground({ 
       title: `${sample(descriptors)}, ${sample(places)}`,
+      price: randomPrice(),
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
+      image: "https://images.unsplash.com/photo-1656931251449-07493b9f6caf?ixid=MnwzMDcyNTZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NTc0NjcxODY\u0026ixlib=rb-1.2.1",
+      discreption: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum aperiam alias debitis esse quia, veniam earum voluptatibus officia obcaecati amet minima minus est voluptatem laborum saepe corporis modi. Neque, atque."
     })
     await camp.save();
   }
